73603af2d7c0310cff1a44987e0a9294c245f6be
Praveen Maurya <mauryapraveenji@gmail.com>
2024-03-05 10:53:54 +0530

    Bullseye part-1 section-1

---
+++ app/src/main/java/com/weandtech/bullseye/GameScreen.kt
@@ -0,0 +1,75 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.weandtech.bullseye.R
+import com.weandtech.bullseye.ui.theme.BullseyeTheme
+
+@Composable
+fun GameScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Spacer(modifier = Modifier.weight(0.5f))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.weight(9f)
+        ) {
+            Text(text = stringResource(R.string.instruction_text))
+            Text(
+                text = stringResource(R.string.target_value_text),
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold)
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.min_value_text),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(start = 16.dp)
+                    )
+                Slider(
+                    value = 0.5f,
+                    valueRange = 0.01f..1f,
+                    onValueChange = {},
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = stringResource(R.string.max_value_text),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(end = 16.dp)
+                )
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(R.string.hit_me_button_text))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    BullseyeTheme {
+        GameScreen()
+    }
+}
