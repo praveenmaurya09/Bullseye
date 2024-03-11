4fb16afc51bb45bc248a6879b1e23b6e06b03323
Praveen Maurya <mauryapraveenji@gmail.com>
2024-03-11 11:19:08 +0530

    Bullseye part-1 section-2 done

--- app/src/main/java/com/weandtech/bullseye/GamePrompt.kt
+++ app/src/main/java/com/weandtech/bullseye/GamePrompt.kt
@@ -1,2 +1,30 @@
 package com.weandtech.bullseye
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun GamePrompt(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+    ) {
+        Text(text = stringResource(id = R.string.instruction_text))
+        Text(
+            text = stringResource(id = R.string.target_value_text),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
--- app/src/main/java/com/weandtech/bullseye/ResultDialog.kt
+++ app/src/main/java/com/weandtech/bullseye/ResultDialog.kt
@@ -1,2 +1,32 @@
 package com.weandtech.bullseye
 
+import android.app.AlertDialog
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun ResultDialog(
+    hideDialog: () -> Unit,
+    sliderValue: Int,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = {
+            hideDialog()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { hideDialog() }
+            ) {
+                Text(text = stringResource(id = R.string.result_dialog_button_text))
+            }
+        },
+        title = { Text(text = stringResource(id = R.string.result_dialog_title)) },
+        text = { Text(text = stringResource(id = R.string.result_dialog_message, sliderValue)) }
+//        text = { Text(text = "The slider's value is $sliderValue") }
+    )
+}
